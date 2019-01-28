6ccbd4ef0b06d911b4893ee3f52c327f6ff90be7
Andrey Helldar <helldar@ai-rus.com>
2019-01-28 15:47:43 +0300

    Added exception

---
+++ src/Exceptions/UrlNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Helldar\LastModified\Exceptions;
+
+use Exception;
+
+class UrlNotFoundException extends Exception
+{
+    public function __construct(string $model, \Throwable $previous = null)
+    {
+        $class_name = get_class($model);
+        $message    = "The URL field not found in model {$class_name}";
+
+        parent::__construct($message, 400, $previous);
+    }
+}
--- src/Services/LastModified.php
+++ src/Services/LastModified.php
@@ -2,42 +2,64 @@
 
 namespace Helldar\LastModified\Services;
 
+use Helldar\LastModified\Exceptions\UrlNotFoundException;
 use Illuminate\Database\Eloquent\Collection;
 
 class LastModified
 {
+    /**
+     * @param \Illuminate\Database\Eloquent\Collection ...$collections
+     *
+     * @return $this
+     * @throws \Helldar\LastModified\Exceptions\UrlNotFoundException
+     */
     public function collections(Collection ...$collections)
     {
-        foreach ((array) $collections as $item) {
-            $this->processCollection($item);
+        foreach ((array) $collections as $collection) {
+            $collection
+                ->each(function ($model) {
+                    $this->models($model);
+                });
         }
 
         return $this;
     }
 
+    /**
+     * @param mixed ...$models
+     *
+     * @return $this
+     * @throws \Helldar\LastModified\Exceptions\UrlNotFoundException
+     */
     public function models(...$models)
     {
         foreach ((array) $models as $model) {
-            $this->processModel($model);
+            $this->process($model);
         }
 
         return $this;
     }
 
-    private function processCollection(Collection $model)
+    /**
+     * @param \Illuminate\Database\Eloquent\Model $model
+     *
+     * @throws \Helldar\LastModified\Exceptions\UrlNotFoundException
+     */
+    private function process($model)
     {
-        $model->each(function ($item) {
-            $this->processModel($item);
-        });
-    }
+        if (!isset($model->url)) {
+            throw new UrlNotFoundException($model);
+        }
 
-    private function processModel($model)
-    {
         $updated_at = $model->updated_at ?? null;
 
         $this->store($model->url, $updated_at);
     }
 
+    /**
+     * @param string $url
+     * @param \DateTimeInterface $updated_at
+     */
     private function store(string $url, \DateTimeInterface $updated_at)
     {
         (new Check)->updateOrCreate($url, $updated_at);
