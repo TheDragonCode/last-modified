2b0eadb878bebb58f8ed2c3912daa2062b301172
Andrey Helldar <helldar@ai-rus.com>
2021-11-17 20:00:25 +0300

    Removed duplication code

--- src/Support/Config.php
+++ src/Support/Config.php
@@ -59,13 +59,18 @@ class Config
     {
         $value = config('last_modified.database.chunk', 1000);
 
-        return abs($value) > 1 ? abs($value) : 1000;
+        return $this->getInteger($value, 1000);
     }
 
     public function cacheTtl(): int
     {
         $value = config('last_modified.cache.ttl', 43200);
 
-        return abs($value) > 1 ? abs($value) : 43200;
+        return $this->getInteger($value, 43200);
+    }
+
+    protected function getInteger(int $value, int $default): int
+    {
+        return abs($value) > 1 ? abs($value) : $default;
     }
 }
